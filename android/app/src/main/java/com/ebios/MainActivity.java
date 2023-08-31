4f58788a1d7b0aee5a104fafd72bfa1065c22d39
Juan-LLantada <56977614+Juan-LLantada@users.noreply.github.com>
2023-08-31 15:58:38 -0600

    Splash

--- android/app/src/main/java/com/ebios/MainActivity.java
+++ android/app/src/main/java/com/ebios/MainActivity.java
@@ -5,7 +5,8 @@ import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
-
+import android.os.Bundle;
+import com.zoontek.rnbootsplash.RNBootSplash;
 public class MainActivity extends ReactActivity {
 
   /**
